bc235929cac1c91dae2b7421a376834512f75416
Scott Doxey <neogeek0101@gmail.com>
2019-01-30 17:47:17 -0500

    Added plugin example.

---
+++ Assets/Plugins/UnityPackageExample/Scripts/Message.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Namespace.UnityPackageExample
+{
+
+    public static class Message
+    {
+
+        public static void HelloWorld()
+        {
+
+            Debug.Log("Hello, World");
+
+        }
+
+    }
+
+}
